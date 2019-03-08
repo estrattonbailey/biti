f1493b4a544317b33c233110f444f211bc79843b
estrattonbailey <ericstrattonbailey@gmail.com>
2019-03-08 11:51:04 -0500

    fully integrate compilation step

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const fs = require('fs-extra')
 const path = require('path')
+const { watch } = require('chokidar')
+const onExit = require('exit-hook')
+const match = require('matched')
+const spitball = require('spitball')
 
-const watch = require('./lib/watch.js')
 const render = require('./lib/render.js')
 
 const cwd = process.cwd()
@@ -18,6 +22,7 @@ module.exports = function biti ({
 }) {
   require('./lib/env.js')({ env, alias })
 
+  const tmp = path.join(__dirname, 'tmp')
   const events = {}
 
   function emit (ev, data) {
@@ -31,28 +36,101 @@ module.exports = function biti ({
 
   return {
     on,
-    render (src, dest) {
-      console.log(cwd, src)
+    async render (src, dest) {
+      try {
+        await spitball({
+          in: path.join(src, '*.js'),
+          out: tmp,
+          env,
+          alias,
+          node: true
+        }).build()
+      } catch (e) {
+        emit('error', e)
+      }
+
       return render(
-        abs(src),
+        tmp,
         abs(dest),
         null,
         { filter, wrap, html },
         emit
-      ).then(() => emit('done'))
+      ).then(() => {
+        fs.removeSync(tmp)
+        emit('done')
+        return
+      })
     },
-    watch (src, dest) {
-      console.log(abs(src))
-      watch(abs(src))
-        .on('change', pages => {
-          render(
-            abs(src),
-            abs(dest),
-            pages,
-            { filter, wrap, html },
-            emit
-          )
+    async watch (src, dest) {
+      await this.render(src, dest)
+
+      let compiler
+      let watcher
+      let renderers = new Map()
+
+      function createCompiler () {
+        const pages = match.sync(path.join(src, '*.js'))
+
+        compiler = spitball(pages.map(page => ({
+          in: page,
+          out: tmp,
+          env,
+          alias,
+          node: true
+        }))).watch((e, stats) => {
+          if (e) emit('error', e)
+
+          if (!watcher) {
+            watcher = watch([ tmp, src ], {
+              ignored: [
+                /chunk-(?:[a-z]|[0-9])+\.js$/,
+                /\.map$/
+              ],
+              ignoreInitial: true
+            })
+              .on('add', async page => {
+                // only add pages added to src
+                if (page.indexOf(src) < 0) return
+
+                emit('add', page)
+
+                await compiler.close()
+                createCompiler()
+              })
+              .on('change', page => {
+                // don't render un-compiled pages
+                if (page.indexOf(tmp) < 0) return
+
+                if (renderers.has(page)) return
+
+                renderers.set(page, (function (p) {
+                  render(
+                    tmp,
+                    abs(dest),
+                    [ p ],
+                    { filter, wrap, html },
+                    emit
+                  ).then(() => {
+                    renderers.delete(p)
+                  })
+                })(page))
+              })
+          }
         })
+      }
+
+      onExit(() => {
+        fs.removeSync(tmp)
+      })
+
+      createCompiler()
+
+      return {
+        async close () {
+          watcher.close()
+          await compiler.close()
+        }
+      }
     }
   }
 }
